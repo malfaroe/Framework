08d7b805baed274931124efa01d6a1d56f846785
malfaroe <mauricio.alfaro.espinoza@gmail.com>
2020-12-20 22:58:33 -0300

    "New feature importance module"

--- src/feature_importance.py
+++ src/feature_importance.py
@@ -11,50 +11,34 @@ import os
 import joblib 
 import glob
 
+import yellowbrick
+from yellowbrick.model_selection import FeatureImportances
+
+import warnings
+warnings.filterwarnings("ignore")
 
 
 ##FEATURE IMPORTANCE
 
-def feat_importance(all_files, X_train):
-    nrows = 2
-    ncols = 1
-    fig, axes = plt.subplots(nrows = nrows, ncols = ncols, sharex="all", figsize=(15,15))
+def feat_importance(all_files, X_train, y_train):
     names_classifiers = []
-    nclassifier = 0
-    # all_files = glob.glob("../models" + "/*.bin")
     for filename in all_files:
-            model = joblib.load(filename)
-            names_classifiers.append((filename, model.best_estimator_))
+        model = joblib.load(filename)
+        names_classifiers.append((filename, model.best_estimator_))
 
-    for row in range(nrows):
-        for col in range(ncols):
-            name = names_classifiers[nclassifier][0]
-            classifier = names_classifiers[nclassifier][1]
-            indices = np.argsort(classifier.feature_importances_)[::-1][:40]
-            g = sns.barplot(y=X_train.columns[indices][:40], 
-            x = classifier.feature_importances_[indices][:40] , 
-            orient='h',ax=axes[row][col])
-            g.set_xlabel("Relative importance",fontsize=12)
-            g.set_ylabel("Features",fontsize=12)
-            g.tick_params(labelsize=9)
-            g.set_title(name + " feature importance")
-            nclassifier += 1
-    plt.show()
+    for name, model in names_classifiers:
+        viz = FeatureImportances(model)
+        viz.fit(X_train, y_train)
+        viz.show()
 
 
 
 if __name__ == "__main__":
     
-    df = pd.read_csv(config.DF)
-    target = config.TARGET
-    num_folds = config.FOLDS
-    kfold = StratifiedKFold(n_splits = num_folds)
-    seed = config.SEED
     X_train =  pd.read_csv("../input/X_train.csv")
     y_train =  pd.read_csv("../input/y_train.csv").values.ravel()
-    scoring = config.SCORING
     all_files = glob.glob("../models/bestModels" + "/*.bin")
-    feat_importance(all_files, X_train)
+    feat_importance(all_files, X_train, y_train)
 
     
     #
