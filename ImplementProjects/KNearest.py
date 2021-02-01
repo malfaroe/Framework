518d779cea65ec8a4c856726ace206d92e6323f5
malfaroe <mauricio.alfaro.espinoza@gmail.com>
2021-02-01 08:18:05 -0300

    Regressor class added to KNearest module

--- ImplementProjects/KNearest.py
+++ ImplementProjects/KNearest.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from statistics import mode
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error 
+
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -23,7 +25,7 @@ that optimize the score
 Note: Distance measures are strongly affected by the scale of the input data.
 Experiment with standardization and other data preparation methods in order to improve results."""
 
-class KNearest():
+class KNearestClassifier():
     def __init__(self):
         pass
     
@@ -32,9 +34,9 @@ class KNearest():
         x2 = np.array(x2)
         return np.sqrt(np.sum((x2 - x1)**2, axis = 1).astype("float"))
 
-    def k_calc(self, df, x, target, k):#Get the k nearest neighbors
+    def k_calc(self, df, x, target, k):#
         df["euc"] = self.euclidean_distance(df.iloc[:, df.columns!= target], x)
-        k_values = list(df.sort_values(by = "euc", ascending = True)[:k][target].values)
+        k_values = list(df.sort_values(by = "euc", ascending = True)[:k][target].values) #Get the k nearest neighbors
         y_k = max(k_values)
         df.drop("euc", axis = 1, inplace = True)
         return y_k
@@ -56,6 +58,42 @@ class KNearest():
             if sc > maximo[1]:
                 maximo = (k+1, sc)
         return maximo
+
+
+class KNearestRegressor():
+    def __init__(self):
+        pass
+    
+    def euclidean_distance(self, x1, x2): #distance between two vectors
+        x1 = np.array(x1)
+        x2 = np.array(x2)
+        return np.sqrt(np.sum((x2 - x1)**2, axis = 1).astype("float"))
+
+    def k_calc(self, df, x, target, k):
+        df["euc"] = self.euclidean_distance(df.iloc[:, df.columns!= target], x)
+        k_values = list(df.sort_values(by = "euc", ascending = True)[:k][target].values)#Get the k nearest neighbors
+        y_k = np.mean(k_values)
+        df.drop("euc", axis = 1, inplace = True)
+        return y_k
+
+    def predict(self, df, x, target, k):
+        y_predict = [self.k_calc(df, x.iloc[i,:], target, k) for i in range(len(x))]
+        return np.array(y_predict)
+
+    def scorer(self, y_pred, y):
+            return mean_squared_error(y_pred, y)
+
+    def k_optimal(self, train, X_test, y_test, target):
+        scores = []
+        for i in range(1, len(X_test)):
+            y_pred = self.predict(train, X_test, target, k = i)
+            scores.append(self.scorer(y_test, y_pred))
+        maximo = (0,0)
+        for k, sc in enumerate(scores):
+            if sc > maximo[1]:
+                maximo = (k+1, sc)
+        return maximo
+    
     
 # Testing
 if __name__ == "__main__":
